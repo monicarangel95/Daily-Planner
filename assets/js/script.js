b1dd0acd491ccb86817014fa87c17e36b4d41b34
Monica Rangel <joanna.monica@yahoo.com>
2020-06-15 20:02:42 -0700

    adding my global variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,24 @@ $(document).ready(function () {
     function getCurrentTime() {
         var today = moment().format("MMMM Do YYYY, h:mm:ss a");
         $("#currentDay").text(today);
-    } 
+    }
+    getCurrentTime();
+    // function to compare hours
+    function compareHour() {
+        var hourCurrent = moment().format("H");
+        console.log(hourCurrent);
+        $(".description").each(function () {
+            var hourSlot = parseInt($(this).attr("id"));
+            if (hourSlot < hourNow) {
+                $(this).addClass("past");
+            } else if (hourSlot === hourCurrent) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+            } else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+            }
+        });
+    }
+    compareHour();
